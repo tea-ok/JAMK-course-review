8b6e1760fff916cf5d8417434470c6cbb2b312c2
Taavi Kalaluka <taavi.k@protonmail.com>
2024-01-06 15:34:10 +0200

    Add AuthService, implement login with backend API call

---
+++ JAMKCourseReviewFrontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:5073/api/users';
+  private _isLoggedIn = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this._isLoggedIn.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http
+      .post<any>(`${this.apiUrl}/login`, { username, password })
+      .pipe(
+        tap((response) => {
+          if (response.success) {
+            this._isLoggedIn.next(true);
+          }
+        })
+      );
+  }
+
+  // TODO: Implement register
+
+  logout(): Observable<any> {
+    return this.http
+      .post<any>(`${this.apiUrl}/logout`, {}, { withCredentials: true })
+      .pipe(
+        tap(() => {
+          this._isLoggedIn.next(false);
+        })
+      );
+  }
+}
--- JAMKCourseReviewFrontend/src/app/modules/login/pages/login/login.component.ts
+++ JAMKCourseReviewFrontend/src/app/modules/login/pages/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  onFormSubmit({ username, password }: { username: string; password: string }) {
+    this.authService.login(username, password).subscribe({
+      next: (response) => {
+        console.log('Login successful', response);
+
+        // Redirect back to search page
+        this.router.navigate(['']);
+      },
+      error: (error) => {
+        console.log('Login failed', error);
+        // TODO:
+        // Handle the error, display warning with message from backend underneath the form
+      },
+    });
+  }
 }
