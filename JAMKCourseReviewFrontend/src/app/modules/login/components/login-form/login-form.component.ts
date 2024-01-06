4bfb9d109eae91527ab02c85a67547b9a548309d
Taavi Kalaluka <taavi.k@protonmail.com>
2024-01-06 14:48:40 +0200

    Create functioning login-form

--- JAMKCourseReviewFrontend/src/app/modules/login/components/login-form/login-form.component.ts
+++ JAMKCourseReviewFrontend/src/app/modules/login/components/login-form/login-form.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
+  @Output() formSubmit = new EventEmitter<{
+    username: string;
+    password: string;
+  }>();
 
+  username: string = '';
+  password: string = '';
+
+  submit() {
+    this.formSubmit.emit({ username: this.username, password: this.password });
+  }
 }
--- JAMKCourseReviewFrontend/src/app/modules/login/login.module.ts
+++ JAMKCourseReviewFrontend/src/app/modules/login/login.module.ts
@@ -5,15 +5,20 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    LoginFormComponent
-  ],
+  declarations: [LoginComponent, LoginFormComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
